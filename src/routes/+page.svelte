ba6061990249e8e9fff95dd603930b8317ac062e
Yoonjae Choi <dbswo9795@gmail.com>
2025-04-24 22:38:38 +0900

    init project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  let count = $state(0);
+</script>
+
+<h1>Hello Svelte World</h1>
+<p>count : {count}</p>
+<div>
+  <button onclick={() => count++}>+</button>
+  <button onclick={() => count--}>-</button>
+</div>
