b477ab4213cacd79cfdd17f4b02475879f5cdf76
Yoonjae Choi <dbswo9795@gmail.com>
2025-05-07 22:48:16 +0900

    style: 모달 버튼 하단 고정

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -12,6 +12,7 @@
 <div class={cn('bg-opacity-50 fixed inset-0 z-50 flex items-center justify-center bg-black/30')}>
   <div
     class={cn(
+      'relative',
       'flex flex-col items-center gap-4',
       'min-h-[200px] w-[calc(100%-2rem)] max-w-md p-4',
       'rounded-lg bg-white',
@@ -23,7 +24,9 @@
       <p class="text-center whitespace-pre-line">{message}</p>
     {/if}
     {#if onClose && buttonText}
-      <button class={cn('mt-4 rounded-md bg-blue-500 px-4 py-2 text-white')} onclick={onClose}>{buttonText}</button>
+      <button class={cn('absolute bottom-4', 'px-4 py-2', 'rounded-md bg-blue-500 text-white')} onclick={onClose}>
+        {buttonText}
+      </button>
     {/if}
   </div>
 </div>
